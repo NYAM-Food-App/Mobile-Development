442844b1fe62ff49594216ee57def0516dc21a80
yoc19 <yohaneschristianto19@gmail.com>
2024-11-29 01:29:36 +0700

    Made Daily Report Card in fragment_home.xml

--- app/src/main/java/com/example/nyam/view/home/HomeFragment.kt
+++ app/src/main/java/com/example/nyam/view/home/HomeFragment.kt
@@ -6,15 +6,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.nyam.R
+import com.example.nyam.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding : FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //Tes ProgressBar onClick aja, bisa dihapus
+        binding.cardDailyReport.progressBarCalories.setOnClickListener {
+            binding.cardDailyReport.progressBarCalories.progress += 10
+        }
+
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
