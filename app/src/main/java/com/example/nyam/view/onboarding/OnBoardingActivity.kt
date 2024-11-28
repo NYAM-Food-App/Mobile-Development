aef7616ef9d38d53b4e6c06671129382152d9c87
yoc19 <yohaneschristianto19@gmail.com>
2024-11-29 04:17:11 +0700

    minor changes

--- app/src/main/java/com/example/nyam/view/onboarding/OnBoardingActivity.kt
+++ app/src/main/java/com/example/nyam/view/onboarding/OnBoardingActivity.kt
@@ -27,15 +27,14 @@ class OnBoardingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityOnboardingBinding.inflate(layoutInflater)
-        enableEdgeToEdge()
         setContentView(binding.root)
+        enableEdgeToEdge()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        supportActionBar?.hide()
 
         binding.btnSkip.setOnClickListener {
             val intent = Intent(this.baseContext, LoginActivity::class.java)
